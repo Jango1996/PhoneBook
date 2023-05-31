84b2426650562f9ec1f5fb0fc2ea88e98a65dea0
Ruslan Shakirov <vikingsonsturm@gmail.com>
2023-05-31 21:08:03 +0300

    view version 2

--- view.py
+++ view.py
@@ -1,100 +1,70 @@
-import controller
-import model 
-
-def menu():
-    while True:
-        print('\nМЕНЮ')
-        print('1 - Показать все контакты')
-        print('2 - Добавить новый контакт')
-        print('3 - Поиск контакта')
-        print('4 - Изменить существующую запись')
-        print('5 - Удалить запись')
-        print('6 - Закрыть программу\n')
-        n = сhecking_the_number(input('Выберите пункт меню: '))
-
-        if n == 1:
-            print(controller.get_data)
-
-        elif n == 2:
-            name = input('Введите имя: ')
-            surname = input('Введите фамилию: ')
-            number = input('Введите номер телефона: ')
-            controller.add_contact(name, surname, number)
-        elif n == 3:
-            search = input('Введите фамилию: ')
-            print(controller.find_contact(surname=search))
-            search = input('Введите имя: ')
-            print(controller.find_contact(name=search))
-            search = input('Введите номер  телефона: ')
-            print(controller.find_contact(number=search))
-
-        elif n == 4:
-            print('1. Найти номер по фамилии.')
-            print('2. Найти номер по имени.')
-            print('3. Поиск по номеру телефона.')
-            change = controller.change_contact(input('Введите номер пункта: '))
-            if change == 1:
-                search = input('Введите фамилию: ')
-                controller.find_contact(surname=search)
-                user_id = input('Введите id записи: ')
-                new_number = input('Введите новый номер телефона: ')
-                controller.change_contact(id=user_id, new_number=new_number)
-
-            elif change == 2:
-                search = input('Введите имя: ')
-                controller.find_contact(name=search)
-                user_id = input('Введите id записи: ')
-                new_number = input('Введите новый номер телефона: ')
-                controller.change_contact(id=user_id, new_number=new_number)
-
-            elif change == 3:
-                search = input('Введите номер телефона: ')
-                controller.find_contact(number=search)
-                user_id = input('Введите id записи: ')
-                new_number = input('Введите новый номер телефона: ')
-                controller.change_contact(id=user_id, new_number=new_number)
-
-            else:
-                print('\nТакого пункта меню не существует.\nВведите цифру, соответствующую пункту меню.')
-
-        elif n == 5:
-            print('1. Найти номер по фамилии.')
-            print('2. Найти номер по имени.')
-            print('3. Поиск по номеру телефона.')
-            deleting = сhecking_the_number(input('Введите номер пункта: '))
-
-            if deleting == 1:
-                search = input('Введите фамилию: ')
-                print(controller.find_contact(surname=search))
-                user_id = input('Введите id записи: ')
-                controller.delete_contact(id=user_id)
-
-            elif deleting == 2:
-                search = input('Введите имя: ')
-                print(controller.find_contact(name=search))
-                user_id = input('Введите id записи: ')
-                controller.delete_contact(id=user_id)
-
-            elif deleting == 3:
-                search = input('Введите номер телефона: ')
-                print(controller.find_contact(number=search))
-                user_id = input('Введите id записи: ')
-                new_number = input('Введите новый номер телефона: ')
-                controller.delete_contact(id=user_id)
-
-            else:
-                print('\nТакого пункта меню не существует.\nВведите цифру, соответствующую пункту меню.')
-
-        elif n == 6:
-            print('Завершение!')
-            break
-        
-        else:
-            print('\nТакого пункта меню не существует.\nВведите цифру, соответствующую пункту меню.')
+def greetings():
+    print('Добро пожаловать в Телефонный справочник!')
 
 
-def сhecking_the_number(num):
-    while num.isdigit() != True:
-        num = input('Введите соответствующий пункт меню: ')
-    return int(num)
+def farewell():
+    print('Вы закончили работу со справочником. До свидания!')
 
+
+def show_menu():
+    main_menu = ['1: Показать все контакты',
+                 '2: Найти контакт',
+                 '3: Добавить новый контакт',
+                 '4: Изменить контакт',
+                 '5: Удалить контакт',
+                 '6: Очистить справочник',
+                 '7: Выйти из приложения']
+    print()
+    print(*main_menu)
+
+
+def show_contacts(pb: list):
+    if not pb:
+        print('\n' + 'Контакты не найдены.')
+    else:
+        print('\n' + 'Найдены следующие контакты:')
+        for i in pb:
+            print(i)
+
+
+def confirm_adding(answer: bool, contact: str):
+    if answer:
+        print(f'Контакт {contact.title()} успешно добавлен в Справочник.')
+    else:
+        print(f'Произошла ошибка. Контакт {contact.title()} не добавлен. Повторите попытку.')
+
+
+def confirm_deleting(answer: bool, contact: str):
+    if answer:
+        print(f'Контакт {contact.title()} успешно удален из Справочника.')
+    else:
+        print(f'Произошла ошибка. Контакт {contact.title()} не удален. Повторите попытку.')
+
+
+def confirm_changing(answer: bool, contact_old: str, contact_new: str):
+    if answer:
+        print(f'Контакт {contact_old.title()} успешно заменен на {contact_new.title()}.')
+    else:
+        print(f'Произошла ошибка. Контакт {contact_old.title()} не изменен.')
+
+
+def confirm_phone_book_clearing(answer: bool):
+    if answer:
+        print('Справочник успешно очищен.')
+    else:
+        print('Произошла ошибка. Справочник не очищен.')
+
+
+def ask_contact_data(request: str):
+    if request == 'to_delete':
+        print('\n' + 'Последовательно введите данные контакта, который хотите удалить. '
+              '(При необходимости сначала воспользуйтесь поиском)')
+    elif request == 'to_change':
+        print('\n' + 'Последовательно введите данные контакта, который хотите изменить. '
+              '(При необходимости сначала воспользуйтесь поиском)')
+    else:
+        print('\n' + 'Последовательно введите новые данные контакта.')
+
+
+def invalid_command():
+    print('Выберите корректную команду:')
